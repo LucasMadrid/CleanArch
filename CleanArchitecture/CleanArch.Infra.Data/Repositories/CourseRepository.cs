339cfbb95d2d62bc6f49e3d5c335cd598dbd6b27
Lucas Madrid <madridlucas75@gmail.com>
2020-07-21 10:38:10 -0300

    Commit implementation of service and Repostory GetCourses method

---
+++ CleanArchitecture/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
